410245d55b09a1c597c2c9a3364d35213ed43abc
Matteo Pompili <matpompili@users.noreply.github.com>
2022-07-29 22:18:40 -0500

    Improve README and setup

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import setup
 with open("requirements.txt") as installation_requirements_file:
     requirements = installation_requirements_file.read().splitlines()
 
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="easypubsub",
     version="0.2.0",
     packages=["easypubsub"],
     url="https://github.com/matpompili/easypubsub",
+    
     author="Matteo Pompili",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -17,6 +21,8 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     description="A simple wrapper around PyZMQ that provides an easy interface to Publish Subscribe.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     install_requires=requirements,
     test_suite="tests",
     package_data={"": ["LICENSE"]},
